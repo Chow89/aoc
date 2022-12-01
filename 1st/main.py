ae8825f24fab3f55205b3bdb931e55f5ddd81fc1
Christian Tessnow <christian_tessnow@yahoo.de>
2022-12-01 18:27:54 +0100

    clean up code

--- 1st/main.py
+++ 1st/main.py
@@ -1,20 +1,16 @@
-import requests
-
 def main():
     with open('1st/inputs.txt') as file:
         lines = file.readlines()
-        index = 0
-        elves = [0]
+        elv, elves = 0, [0]
         for line in lines:
             if line == '\n':
-                index = index + 1
+                elv += 1
                 elves.append(0)
                 continue
-            elves[index] = elves[index] + int(line)
+            elves[elv] += int(line)
         elves.sort(reverse=True)
-        print("Top 1:")
-        print(elves[0])
-        print("Top 3 sum:")
-        print(sum(elves[0:3]))
+        print("Top: " + str(elves[0]))
+        print("Top 3: " + str(sum(elves[0:3])))
+
 
 main()
