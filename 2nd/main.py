5a1e9d784402f2e759e1cfb3db1ff99cbd322af7
Christian Tessnow <christian2@hepster.com>
2022-12-02 11:43:12 +0100

    dec 2nd

---
+++ 2nd/main.py
@@ -0,0 +1,55 @@
+def main():
+    with open('2nd/inputs.txt') as file:
+        lines = file.readlines()
+        sum = 0
+        for line in lines:
+            match line.strip():
+                case "A X":
+                    sum += 1 + 3
+                case "A Y":
+                    sum += 2 + 6
+                case "A Z":
+                    sum += 3 + 0
+                case "B X":
+                    sum += 1 + 0
+                case "B Y":
+                    sum += 2 + 3
+                case "B Z":
+                    sum += 3 + 6
+                case "C X":
+                    sum += 1 + 6
+                case "C Y":
+                    sum += 2 + 0
+                case "C Z":
+                    sum += 3 + 3
+        print(sum)
+
+def main2():
+    with open('2nd/inputs.txt') as file:
+        lines = file.readlines()
+        sum = 0
+        for line in lines:
+            match line.strip():
+                case "A X":
+                    sum += 3 + 0
+                case "A Y":
+                    sum += 1 + 3
+                case "A Z":
+                    sum += 2 + 6
+                case "B X":
+                    sum += 1 + 0
+                case "B Y":
+                    sum += 2 + 3
+                case "B Z":
+                    sum += 3 + 6
+                case "C X":
+                    sum += 2 + 0
+                case "C Y":
+                    sum += 3 + 3
+                case "C Z":
+                    sum += 1 + 6
+        print(sum)
+
+
+main()
+main2()
