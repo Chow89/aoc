be9295a099779acbba054c28ab0420a2fa278303
Christian Tessnow <christian_tessnow@yahoo.de>
2022-12-03 15:12:09 +0100

    3rd dec

---
+++ 3rd/main.py
@@ -0,0 +1,42 @@
+def main():
+    with open('3rd/inputs.txt') as file:
+        lines = file.readlines()
+        itemTypes = [];
+        for line in lines:
+            halfLength = int(len(line.strip()) / 2)
+            first, second = line[:halfLength], line[halfLength:]
+            for letter in first:
+                if letter in second:
+                    itemTypes.append(letter)
+                    break
+        sum = 0
+        for char in itemTypes:
+            if ord(char) >= 97:
+                sum += ord(char) - 96
+            else:
+                sum += ord(char) - 38
+        print(sum)
+
+def main():
+    with open('3rd/inputs.txt') as file:
+        lines = file.readlines()
+        groups = int(len(lines) / 3)
+        itemTypes = [];
+        for i in range(0, groups):
+            startLine = i * 3
+            first = lines[startLine]
+            second = lines[startLine + 1]
+            third = lines[startLine + 2]
+            for letter in first:
+                if letter in second and letter in third:
+                    itemTypes.append(letter)
+                    break
+        sum = 0
+        for char in itemTypes:
+            if ord(char) >= 97:
+                sum += ord(char) - 96
+            else:
+                sum += ord(char) - 38
+        print(sum)
+
+main()
