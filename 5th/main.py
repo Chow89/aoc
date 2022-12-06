59dde41870f63a133785b98082129b2638cff07c
Christian Tessnow <christian_tessnow@yahoo.de>
2022-12-06 19:06:56 +0100

    add stack parsing

--- 5th/main.py
+++ 5th/main.py
@@ -1,23 +1,18 @@
 def main():
     with open('5th/inputs.txt') as file:
         lines = file.readlines()
-        stacks = [[''],
-                  ['H','B','V','W','N','M','L','P'],
-                  ['M','Q','H'],
-                  ['N','D','B','G','F','Q','M','L'],
-                  ['Z','T','F','Q','M','W','G'],
-                  ['M','T','H','P'],
-                  ['C','B','M','J','D','H','G','T'],
-                  ['M','N','B','F','V','R'],
-                  ['P','L','H','M','R','G','S'],
-                  ['P','D','B','C','N']
-                  ]
+        stacks = [[], [], [], [], [], [], [], [], []]
         instructions = False
         for line in lines:
             if line == '\n':
+                stacks.insert(0, [])
                 instructions = True;
             elif not instructions:
-                continue
+                crates = line[1::4]
+                if crates[0] != '1':
+                    for i in range(0, len(crates)):
+                        if crates[i] != ' ':
+                            stacks[i].insert(0, crates[i])
             else:
                 _, elements, _, origin, _, destination = line.split(' ')
                 elements = int(elements)
@@ -35,23 +30,18 @@ def main():
 def main2():
     with open('5th/inputs.txt') as file:
         lines = file.readlines()
-        stacks = [[''],
-                  ['H','B','V','W','N','M','L','P'],
-                  ['M','Q','H'],
-                  ['N','D','B','G','F','Q','M','L'],
-                  ['Z','T','F','Q','M','W','G'],
-                  ['M','T','H','P'],
-                  ['C','B','M','J','D','H','G','T'],
-                  ['M','N','B','F','V','R'],
-                  ['P','L','H','M','R','G','S'],
-                  ['P','D','B','C','N']
-                  ]
+        stacks = [[], [], [], [], [], [], [], [], []]
         instructions = False
         for line in lines:
             if line == '\n':
+                stacks.insert(0, [])
                 instructions = True;
             elif not instructions:
-                continue
+                crates = line[1::4]
+                if crates[0] != '1':
+                    for i in range(0, len(crates)):
+                        if crates[i] != ' ':
+                            stacks[i].insert(0, crates[i])
             else:
                 _, elements, _, origin, _, destination = line.split(' ')
                 elements = int(elements)
